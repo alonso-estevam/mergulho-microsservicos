a3c9af4864559d37693f2f6973bc3fd39e134dda
Alonso Estevam <alonso_estevam@outlook.com>
2025-07-27 21:15:40 -0300

    feat: add domain models

---
+++ courier-management/src/main/java/com/algaworks/algadelivery/courier/management/domain/model/AssignedDelivery.java
@@ -0,0 +1,30 @@
+package com.algaworks.algadelivery.courier.management.domain.model;
+
+import lombok.*;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class AssignedDelivery {
+
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    private OffsetDateTime assignedAt;
+
+    // private factory porque só o agregado (Courier) deve poder criar uma instância de AssignedDelivery
+    static AssignedDelivery pending(UUID deliveryId) {
+
+        AssignedDelivery delivery = new AssignedDelivery();
+
+        delivery.setId(deliveryId);
+        delivery.setAssignedAt(OffsetDateTime.now());
+
+        return delivery;
+    }
+
+}
